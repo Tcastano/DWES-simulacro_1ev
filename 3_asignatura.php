2accf13df9f9051c36101546ddd787a2c978bc87
Tizziano Nahuel <Nahuel210403@gmail.com>
2022-11-30 12:48:11 +0100

    clase

--- 3_asignatura.php
+++ 3_asignatura.php
@@ -3,3 +3,61 @@
 /**
  * Clase Asignatura
  */
+class Asignatura{
+    protected $nombre;
+    protected $horas;
+    protected $profesor;
+
+    function __construct($nombre,$horas,$profesor)
+    {
+        $this->nombre =$nombre;
+        $this->horas=$horas;
+        $this->profesor=$profesor;
+
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+
+    public function getHoras(){
+        return $this->horas;
+    }
+
+    public function setHoras($horas){
+        $this->horas = $horas;
+    }
+
+    public function getProfesor(){
+        return $this->profesor;
+    }
+
+    public function setProfesor($profesor){
+        $this->profesor = $profesor;
+    }
+
+    public function __toString()
+    {
+        return "Nombre: {$this->nombre}, Horas: {$this->horas}, Profesor: {$this->profesor}";
+    }
+
+    public function toArray()
+    {
+        $arrayAsignatura["nombre"] = $this ->nombre;
+        $arrayAsignatura["horas"] = $this ->horas;
+        $arrayAsignatura["profesor"] = $this ->profesor;
+        
+        $asignaturaEncoded = json_encode($arrayAsignatura,JSON_UNESCAPED_UNICODE);
+        file_put_contents('./datos.json',$asignaturaEncoded);
+
+        return $asignaturaEncoded;
+    }
+
+
+}
+
+
--- 3_controlador.php
+++ 3_controlador.php
@@ -3,5 +3,24 @@
 /**
  * Controlador del formulario
  */
+ require_once("./3_asignatura.php");
 
- file_put_contents("./3_datos.json","hola mundo");
+$asignatura =  new Asignatura($_POST['nombre'],$_POST['horas'],$_POST['profesor']); 
+
+file_put_contents("./3_datos.json",$asignatura->toArray());
+
+$totalAsignaturas = file_get_contents("./3_datos.json");
+$totalAsignaturasDecoded = json_decode($totalAsignaturas, true, 512, JSON_UNESCAPED_UNICODE);
+
+if (!empty($totalAsignaturasDecoded))
+{
+    foreach ($totalAsignaturasDecoded as $key => $value) 
+    {
+       echo ($key. ':' .$value. "<br>");
+    }
+}
+
+
+
+
+
